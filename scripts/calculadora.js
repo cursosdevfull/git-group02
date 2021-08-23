2aa662ddd67dec14a5ccef8366ff12540338874b
Sergio <sergiohidalgocaceres@gmail.com>
2021-08-22 19:24:59 -0500

    agregada validación de números ingresados

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -1,2 +1,6 @@
 const numero1 = prompt('Digite el primer número: ');
-const numero1 = prompt('Digite el segundo número: ');
+const numero2 = prompt('Digite el segundo número: ');
+
+if (isNaN(numero1) || isNaN(numero2)) {
+  console.log('Ingrese números y no cadenas');
+}
